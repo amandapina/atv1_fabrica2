0cf723713e79aebea398a976c04ba1f7fad0b564
amandapina <amandapazeredo@gmail.com>
2023-09-18 23:30:56 -0300

    cartao de visita versao1

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import './Cartao';
+import Cartao from './Cartao';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='quadro'>
+        <h1>AMANDA</h1>
+      <Cartao></Cartao>
+      </div>
+      
+      
+   
+    </div>
+  );
+}
+
+export default App;
+
+//
---
+++ src/Cartao.js
@@ -0,0 +1,15 @@
+import React from 'react' ;
+
+function Cartao()
+{
+    return(
+        <div className = "cartao">
+        <img src = "https://i.pinimg.com/474x/77/43/5a/77435a8b981785677686fbce0bc7e377.jpg" alt = ""></img>
+               <p>Olá, eu sou a amanda e não sei se programo em JavaScript</p>
+     
+        </div>
+    );
+}
+
+export default Cartao;  
+   
