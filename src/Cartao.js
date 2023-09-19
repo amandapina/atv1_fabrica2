ffae869a92f4dac78e738dd56b1aa63011cd33c0
amandapina <amandapazeredo@gmail.com>
2023-09-18 23:36:13 -0300

    mudança de texto

--- src/Cartao.js
+++ src/Cartao.js
@@ -5,7 +5,7 @@ function Cartao()
     return(
         <div className = "cartao">
         <img src = "https://i.pinimg.com/474x/77/43/5a/77435a8b981785677686fbce0bc7e377.jpg" alt = ""></img>
-               <p>Olá, eu sou a amanda e não sei se programo em JavaScript</p>
+               <p>Olá, eu sou a amanda e programo em JavaScript</p>
      
         </div>
     );
